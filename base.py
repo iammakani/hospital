b54638a9daccf34a8e388ba3f886355dce81a97c
d.kinzibaev <d.kinzibaev@rt-solar.ru>
2024-06-07 21:12:45 +0300

    version 2

--- base.py
+++ base.py
@@ -22,8 +22,8 @@ class Base:
         return False
 
     def get_status_digital(self, patient_id):
-        # if int(patient_id)-1 < len(self.patients) and self.patients[int(patient_id)-1]:
-        return int(self.patients[patient_id-1])
+        if int(patient_id)-1 < len(self.patients) and self.patients[int(patient_id)-1] is not None:
+            return int(self.patients[patient_id-1])
 
     def get_status_human_readable(self, patient_id):
         status_digital = self.get_status_digital(patient_id)
